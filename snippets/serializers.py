80d4553c2bc30c514f2828a715d7d9ca96e8faa1
shizhijie <739698292@qq.com>
2018-07-05 17:51:27 +0800

    'restframework'

---
+++ snippets/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    # 利用字段标志控制序列化器渲染到HTML页面时的的显示模板
+    code = serializers.CharField(style={'base_template': 'textarea.html'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
+
+    # 给定经过验证的数据，创建并返回一个新的 Snippet 实例
+    def create(self, validated_data):
+        return Snippet.objects.create(**validated_data)
+
+    # 给定经过验证的数据，更新并返回一个已经存在的 Snippet 实例
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
